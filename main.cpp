f50b5cc4810950c8d26e5579510acd70c2312a9a
Shirley <1184793244@qq.com>
2023-11-30 15:48:26 +0800

    add Key Event demo

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 #include <QDebug>
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QLabel>
+#include <QMouseEvent>
+#include <QString>
+#include <QWidget>
 
 void testQChar(){
     //qt中的控件都是叫做Qxxx的，QT中的字符串就是QString，里面存储的都是一个个的QChar，可以使用[]或者at来获取指定索引的内容
@@ -69,6 +72,56 @@ void QStringTest(){
     qDebug()<< "" <<N;
 }
 
+/**
+ * @brief The EventLabel class 继承自QLabel，QLabel最终是继承自QWidget，QWidget中定义了很多protected的virtual的函数，例如鼠标按下事件，键盘按下事件，子类可以重写去实现这些方法
+ */
+class EventLabel: public QLabel
+{
+protected:
+    void mouseMoveEvent(QMouseEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent* event);
+};
+
+/**
+ * @brief EventLabel::mouseMoveEvent 重写鼠标移动事件
+ * @param event
+ */
+void EventLabel::mouseMoveEvent(QMouseEvent *event){
+    EventLabel::setText(QString("<center><h1>Move: (%1, %2)</h1></center>").arg(QString::number(event->x()),QString::number(event->y())));
+}
+
+/**
+ * @brief EventLabel::mousePressEvent 重写鼠标按下事件
+ * @param event
+ */
+void EventLabel::mousePressEvent(QMouseEvent *event){
+    EventLabel::setText(QString("<center><h1>Press: (%1, %2)</h1></center>").arg(QString::number(event->x()),QString::number(event->y())));
+}
+
+/**
+ * @brief EventLabel::mouseReleaseEvent 重写鼠标释放事件
+ * @param event
+ */
+void EventLabel::mouseReleaseEvent(QMouseEvent *event){
+    QString msg;
+    msg.sprintf("<center><h1>Release: (%d, %d)</h1></center>",
+                event->x(), event->y());
+    this->setText(msg);
+}
+
+//处理键盘按下Tab事件并消费掉
+// bool CustomWidget::event(QEvent e){
+//     if(e.type()==QEvent::KeyPress){
+//         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(e);
+//         if(keyEvent == Qt::Key_Tab){
+//             qDebug<<"press tab...";
+//             return  true;
+//         }
+//     }
+//     return QWidget::event(e);
+// }
+
 int main(int argc, char *argv[])
 {
     //ui显示跟实际车机窗口显示不一致，有时候车机显示不完全。 添加这句话，就可以让窗口显示跟ui显示的一致。
@@ -87,6 +140,13 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.testPushButton();
     w.show();
+
+    EventLabel *label = new EventLabel;
+    label->setMouseTracking(true);//设置一开始就追踪鼠标，这个值默认false：点击一下之后才开始追踪鼠标
+    label->setWindowTitle("MouseEvent Demo");
+    label->resize(300, 200);
+    label->show();
+
 
     return a.exec();
 }
